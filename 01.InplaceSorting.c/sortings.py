7db5012658f1307d7032230936fecb3d72f7eff8
Morev H. <garri.morev@gmail.com>
2026-05-04 09:09:20 +0300

    Добавлены 2 сортировки и тесты к ним

--- 01.InplaceSorting.c/sortings.py
+++ 01.InplaceSorting.c/sortings.py
@@ -17,3 +17,46 @@ def builtin_sort(data):
         # TODO: скормить корректно, или оставить эту идею, и
         # сделать less & swap
         data._array.sort()
+
+
+def bubble_sort(data):
+    def bubble(data):
+        sorted = False
+        while not sorted:
+            sorted = True
+            for i in range(len(data)-1):
+                if data[i] > data[i + 1]: 
+                    data[i], data[i + 1] = data[i + 1], data[i]
+                    sorted = False
+    if isinstance(data, list):
+        bubble(data)
+    else:
+        bubble(data._array)
+
+def merge_sort(data):
+    
+    
+    def _merge(left, right):
+        merged = []
+        i = j = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                j += 1
+                
+        merged.extend(left[i:])
+        merged.extend(right[j:])
+        return merged
+    
+
+    if len(data) <= 1:
+        return data[:]
+    
+    mid = len(data) // 2
+    left = merge_sort(data[:mid])
+    right = merge_sort(data[mid:])
+    return _merge(left, right)
