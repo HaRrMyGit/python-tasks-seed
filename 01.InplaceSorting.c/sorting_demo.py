35229f57d1e27d70ea26b19b4dc945dbe9060ac8
Morev H. <garri.morev@gmail.com>
2026-05-04 09:51:52 +0300

    пытки

--- 01.InplaceSorting.c/sorting_demo.py
+++ 01.InplaceSorting.c/sorting_demo.py
@@ -16,25 +16,25 @@ if __name__ == "__main__":
     data_builtin: CountingList[CountingOrdered[int]] = CountingList(
         [CountingOrdered(e) for e in raw_data]
     )
-    sortings.builtin_sort(data_builtin)
+    sortings.bubble_sort(data_builtin)
     print(
         f"Builtin Comps: {CountingOrdered.comparisons()}, Swaps: {CountingList.likely_swaps()}"
     )
     
     
-    data_bubble: CountingList[CountingOrdered[int]] = CountingList(
-        [CountingOrdered(e) for e in raw_data]
-    )
-    sortings.bubble_sort(data_bubble)
-    print(
-        f"Bubble Comps: {CountingOrdered.comparisons()}, Swaps: {CountingList.likely_swaps()}"
-    )
+    # data_bubble: CountingList[CountingOrdered[int]] = CountingList(
+    #     [CountingOrdered(e) for e in raw_data]
+    # )
+    # sortings.bubble_sort(data_bubble)
+    # print(
+    #     f"Bubble Comps: {CountingOrdered.comparisons()}, Swaps: {CountingList.likely_swaps()}"
+    # )
 
 
-    data_merge: CountingList[CountingOrdered[int]] = CountingList(
-        [CountingOrdered(e) for e in raw_data]
-    )
-    sortings.merge_sort(data_merge)
-    print(
-        f"Merge Comps: {CountingOrdered.comparisons()}, Swaps: {CountingList.likely_swaps()}"
-    )
+    # data_merge: CountingList[CountingOrdered[int]] = CountingList(
+    #     [CountingOrdered(e) for e in raw_data]
+    # )
+    # sortings.merge_sort(data_merge)
+    # print(
+    #     f"Merge Comps: {CountingOrdered.comparisons()}, Swaps: {CountingList.likely_swaps()}"
+    # )
