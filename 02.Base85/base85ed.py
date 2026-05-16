edd35f7bc83c98df37c65aa7b1a1afe3a3fe6874
Morev H. <garri.morev@gmail.com>
2026-05-16 07:35:29 +0300

    Реализация декодера и енкодера

--- 02.Base85/base85ed.py
+++ 02.Base85/base85ed.py
@@ -19,14 +19,14 @@ def encode(b: bytes):
         return b''
     result = []
     for i in range(0, len(b), 4):
-        chunk = b[i:min(i+4,len(b))].ljust(4, b'\x00')
+        chunk = b[i : i + 4].ljust(4, b'\x00')
         num = int.from_bytes(chunk, 'big')
         encodedChunk = []
         for _ in range(5):  
             encodedChunk.append(num % 85)
             num //= 85
         encodedChunk.reverse()
-        result.extend([ALPHABET[d] for d in encodedChunk[:1 + min(4, len(b)) - 1]])
+        result.extend([ALPHABET[d] for d in encodedChunk[: 1 + min(4, len(b) - i)]])
     return (''.join(result)).encode('ascii')
 
 
