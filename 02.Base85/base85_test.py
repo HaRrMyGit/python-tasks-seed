b47549f7aa071c65f47f074e2636a74975b18c43
Morev H. <garri.morev@gmail.com>
2026-05-16 16:48:37 +0300

    Реализация декодера и енкодера

--- 02.Base85/base85_test.py
+++ 02.Base85/base85_test.py
@@ -21,7 +21,7 @@ def test_multiple_chunks_encode():
     Test longer encodes
     """
     assert base85ed.encode(b"Hello World!") == b"NM&qnZy;B1a%^NF"
-    assert base85ed.encode(b"12345") == b"F)}kWH2"
+    assert base85ed.encode(b"1234567890") == b"F)}kWH8MesIWV"
 
 def test_shorts_decode():
     """
@@ -30,20 +30,20 @@ def test_shorts_decode():
     assert base85ed.decode(b"") == b""
     assert base85ed.decode(b"F#") == b"1"
     assert base85ed.decode(b"F){") == b"12"
-    assert base85ed.decode(b"F) {") == b"12"
     assert base85ed.decode(b"F)}kW") == b"1234"
 
 def test_multiple_chunks_decode():
     """
     Test longer decodes
     """
-    assert base85ed.decode(b"F)}kWH2") == b"12345"
+    assert base85ed.decode(b"F)}kWH8MesIWV") == b"1234567890"
     assert base85ed.decode(b"NM&qnZy;B1a%^NF") == b"Hello World!"
 
 def test_raw_decodes():
     """
     Test decodes with unnesessary whitespaces
     """
+    assert base85ed.decode(b"F) {") == b"12"
     assert base85ed.decode(b"NM&qnZy;B1\na%^NF") == b"Hello World!"
     assert base85ed.decode(b"NM&qnZ y;B1a %^NF  ") == b"Hello World!"
 
